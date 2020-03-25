ae3ba7ee4b9366839604250f2dbca92382a73ce6
Lilah <lilahb44@gmail.com>
2020-03-25 13:58:06 +0200

    Allow cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const jwt = require("express-jwt");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(express.json());
 app.use(
   jwt({
