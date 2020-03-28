de8801b3e76f0de6fd8c39b45a01af2cf61ec50f
Lilah <lilahb44@gmail.com>
2020-03-28 17:08:08 +0300

    Fix groups route

--- routes/groups.js
+++ routes/groups.js
@@ -7,11 +7,10 @@ router.post(
   asyncHandler(async (req, res) => {
     const userIdFromToken = req.user.sub;
 
-    const [
-      result
-    ] = await asyncQuery("select id, name from groups where userId = ?", [
-      userIdFromToken
-    ]);
+    const result = await asyncQuery(
+      "select id, name from groups where userId = ?",
+      [userIdFromToken]
+    );
 
     res.json(result);
   })
