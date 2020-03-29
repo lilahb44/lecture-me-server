06c8ac0fcfd0215bfd16576c05ec3a06aa7bfe11
Lilah <lilahb44@gmail.com>
2020-03-29 16:33:54 +0300

    Handle insert new group

--- routes/groups.js
+++ routes/groups.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const asyncHandler = require("express-async-handler");
 const { asyncQuery } = require("../providers/mysqlPool");
+const validate = require("../middlewares/validate");
+const { check } = require("express-validator");
 
 router.post(
   "/groups",
@@ -27,6 +29,29 @@ router.delete(
     );
 
     res.json(result.affectedRows === 1);
+  })
+);
+
+router.put(
+  "/groups",
+  validate([check("name").isLength({ min: 1, max: 20 })]),
+  asyncHandler(async (req, res) => {
+    const name = req.body.name;
+    const userIdFromToken = req.user.sub;
+
+    const result = await asyncQuery("INSERT INTO groups SET ?", {
+      name,
+      userId: userIdFromToken
+    }).catch(err => {
+      if (err.code !== "ER_DUP_ENTRY") throw err;
+
+      return err.code;
+    });
+
+    if (result === "ER_DUP_ENTRY")
+      return res.status(400).json({ error: "Name already exists." });
+
+    res.json(result.insertId);
   })
 );
 
